bdd2cac3c43a011896a22919bdebf7902a7a3515
leulkashay <leulkashay2012@gmail.com>
2022-09-08 02:54:54 -0700

    Task 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0(succes)
+ */
+
+int main(void)
+{
+	printf("\"with proper grammar, butthe outcome is a piece of art,");
+	return (0);
+}
