417bc82b3f6103b33fd215d65e968aa696d2d741
leulkashay <leulkashay2012@gmail.com>
2022-09-08 02:15:50 -0700

    Task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* c program that prints 
+ * Programing is like building a multilingual puzzel
+ */
+int main(void){
+
+	puts("Programing is like building a multilingual puzzel");
+
+	return 0;
+}
