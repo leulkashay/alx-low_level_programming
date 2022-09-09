c877c7628972b722bf48be7cb170cb71354c96ce
leulkashay <leulkashay2012@gmail.com>
2022-09-09 02:24:01 -0700

    Task 0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,8 +16,9 @@ int main(void)
 	
 	if(n > 0)
 	{
-		printf("%d is positive", n);
-	}else if(n == 0){
+		printf("%d is positive\n", n);
+	}else if(n == 0)
+	{
 		printf("%d is zero\n", n);
 	}else if(n < 0)
 	{
