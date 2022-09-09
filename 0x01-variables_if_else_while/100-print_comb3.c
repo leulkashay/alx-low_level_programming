04331d9d6a120618142be978c89ac0abe3e7d711
leulkashay <leulkashay2012@gmail.com>
2022-09-09 11:27:05 -0700

    Task 10. Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@ int main(void)
 {
 	int x, y;
 
-	for (x = 0; x < 10; x++)
+	for (x = 0; x < 9; x++)
 	{
-		for (y = 0; y < 10; y++)
+		for (y = x + 1; y < 10; y++)
 		{
 			putchar((x % 10) + '0');
 			putchar((y % 10) + '0');
 
-			if (x == 9 && y == 9)
+			if (x == 8 && y == 9)
 				continue;
 
 			putchar(',');
