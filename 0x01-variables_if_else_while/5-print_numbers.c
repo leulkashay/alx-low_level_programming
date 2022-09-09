edef9cfeae3931f34b26e47acaf4e0351883b010
leulkashay <leulkashay2012@gmail.com>
2022-09-09 01:09:02 -0700

    Task 5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return : Always 0(succes)
+ */
+int main(void)
+{
+	int i;
+
+	while(i < 10)
+	{
+		printf("%d", i);
+
+		i++;
+	}
+	printf("\n");
+
+	return(0);
+}
