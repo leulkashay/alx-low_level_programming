ea73f3a750e15e38b313231381171565006344c9
leulkashay <leulkashay2012@gmail.com>
2022-09-13 07:28:35 -0700

    Task 9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* times_table - Prints the 9 time table, starting with 0.
+*/
+
+void times_table(void)
+{
+	int num, mul, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mul = 1; mul <= 9; mul++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mul;
+
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((prod / 10) + '0');
+			}
+
+			_putchar((prod % 10( + '0');
+		}
+		_putchar('\n');
+	}
+}
