a4e2eb1154ba03333f1f42f8df053838ac44b8a5
leulkashay <leulkashay2012@gmail.com>
2022-09-14 03:27:10 -0700

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,17 +8,17 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
-	
+
 	i = 0;
-	
+
 	while (i < 10)
-        {
-		i++; /*putchar(i);*/
+	{
+		i++;	/* putchar(i); */
 	}
-	
+
 	printf("Infinite loop avoided! \\o/\n");
-	
+
 	return (0);
 }
