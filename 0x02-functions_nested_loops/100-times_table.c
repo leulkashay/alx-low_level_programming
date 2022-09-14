986819bcfd5d98da5a9b082b6877a4ad5229ed11
leulkashay <leulkashay2012@gmail.com>
2022-09-13 17:09:30 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,8 +4,10 @@
 * print_times_table - prints the n times table, starting with 0
 * @n: number of the times table
 */
+
 void print_times_table(int n)
 {
+	
 	int i, j, k;
 	
 	if (n >= 0 && n <= 15)
@@ -41,7 +43,6 @@ void print_times_table(int n)
 					_putchar((k % 10) + '0');
 				}
 			}
-			
 			_putchar('\n');
 		}
 	}
