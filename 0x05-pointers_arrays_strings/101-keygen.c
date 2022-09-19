3fd42a2ac30931c57bd48d8dc11a1c0e8e74606b
leulkashay <leulkashay2012@gmail.com>
2022-09-19 11:01:59 -0700

    Task 11

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - generate random password
+*
+* Return: 0(success)
+*/
+
+int main(void)
+{
+	int pass, sum;
+
+	srand(time(NULL));
+
+	sum = 0;
+
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+
+		printf("%c", pass);
+	}
+	printf("%c", 2772 - sum);
+
+	return (0);
+}
