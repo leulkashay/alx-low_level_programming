09cce40cc53217263069838c3e7cc8bc91bb3a14
leulkashay <leulkashay2012@gmail.com>
2022-09-19 01:45:19 -0700

    Task 3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* _puts - Prints string followed by new line.
+* @str: pointer parameter
+*
+* Return: Nothing
+*/
+
+void _puts(char *str)
+{
+	int index;
+
+	index = 0;
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+
+		index++;
+
+		*str++;
+	}
+	_putchar('\n');
+}
