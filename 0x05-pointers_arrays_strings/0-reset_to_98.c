082c2451abb8d6a5effe1cf4b986bca10f01af66
leulkashay <leulkashay2012@gmail.com>
2022-09-18 22:30:10 -0700

    Task 0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Reset value of variable to 98
+*
+* Return: Noting
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef
+#define
+
+void reset_to_98(int *n);
+
+#endif
