93bb6b61d1c5570546c95e581face2b2a95bc1ea
leulkashay <leulkashay2012@gmail.com>
2022-09-19 04:39:42 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* puts_half - Prints half of a string.
+* @str: string pointer
+*
+* Return: void
+*/
+
+void puts_half(char *str)
+{
+	int i, last;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	last = (i + 1) / 2;
+
+	for (last = 0; str[i]; last++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
