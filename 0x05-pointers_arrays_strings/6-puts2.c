1d988d1103f95e5b7878f13591669b116234d383
leulkashay <leulkashay2012@gmail.com>
2022-09-19 02:53:19 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* puts2 -  Prints half of the string
+* @str: string pointer
+*
+* Return: Nothing
+*/
+
+void puts2(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		if(index % 2 == 0)
+		{
+			_putchar(str[index]);
+		}
+	}
+	
+	_putchar('\n');
+}
