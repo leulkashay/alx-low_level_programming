ccc4d01c60afab46ca3835e75843dd2eb99dc726
leulkashay <leulkashay2012@gmail.com>
2022-09-22 06:08:21 -0700

    Capital

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *s)
 
 	if (s[0] >= 'a' && s[0] <= 'z')
 		s[0] = s[0] - 32;
-	while (s[i] != 0)
+	while (s[i] != '\0')
 	{
 		if ((s[i - 1] == '' || s[i - 1] == '\t' ||
 				s[i - 1] == '\n' || s[i - 1] == ',' ||
