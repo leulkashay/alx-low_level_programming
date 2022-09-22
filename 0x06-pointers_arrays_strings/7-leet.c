eeef8f89e6ccc27f883d73115ffe88e76819115d
leulkashay <leulkashay2012@gmail.com>
2022-09-21 19:14:43 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* leet - encoded a string
+* @s: string
+* Retrun: return encoded string
+*/
+
+char *leet(char *s)
+{
+	char *cp = s;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {'4', '3', '0', '7', '1'};
+	unsigned int i;
+	
+	while (*s)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if (*s == key[i] || *s == key[i] + 32)
+			{
+				*s = 48 + value[i];
+			}
+		}
+		s++;
+	}
+	return (s);
+}	
