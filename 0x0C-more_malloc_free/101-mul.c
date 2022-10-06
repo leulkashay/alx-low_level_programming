a16950a8504f66bd3605def73af8477ccd8a35ea
leulkashay <leulkashay2012@gmail.com>
2022-10-06 09:31:35 -0700

    task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,144 +1,27 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
-* _print - moves a string one place to the left
-* @str: string to move
-* @l: size of string
-* Return: void
-*/
-
-void _print(char *str, int l)
-{
-	int i, j;
-
-	while (i < l)
-	{
-		if (str[i] != '0')
-			j = 1;
-		if (j || i == l - 1)
-			_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
-	free(str);
-}
-
-/**
-* mul - multplies a char with a string
-* @: char to multiply
-* @num: string to multiply
-* @num_index: last non null index of num
-* @dest: destination of multiplcation
-* @dest_index: highest index to start addition
-* Return: pointer to dest, or null on failure
-*/
-char *mul(char n, char *num, int num_index, char *dest, int dest_index)
-{
-	int j, k, mul, mulrem, add, addrem;
-
-	mulrem = addrem = 0;
-
-	for (j = num_index, k = dest_index; j >= 0; j--, k--)
-	{
-		mul = (n - '0') * (num[j] - '0') + mulrem;
-		mulrem = mul / 10;
-		add = (dest[k] - '0') + (mul % 10) + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	for (addrem += mulrem; k >= 0 && addrem; k--)
-	{
-		add = (dest[k] - '0') + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	if (addrem)
-	{
-		return (NULL);
-	}
-	return (dest);
-}
-/**
-* check_for_digits - checks if it's digit
-* @av: pointer parameter
-* Return: 0 if digits, 1 if not
-*/
-int check_for_digits(char **av)
-{
-	int i, j;
-
-	for (i = 1; i < 3; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			if (av[i][j] < '0' || av[i][j] > '9')
-				return (1);
-		}
-	}
-	return (0);
-}
-
-/**
-* init - initialze a string
-* @str: string to initialze
-* @l: length of string
-* Return: void
-*/
-void init(char *str, int l)
-{
-	int i;
-
-	for (i = 0; i < l; i++)
-	{
-		str[i] = '0';
-	}
-	str[i] = '\0';
-}
-
-/**
-* main - multiply two numbers
+* mul - a function to multiply an argument
+* @argv: an array of argument
 * @argc: number of argument
-* @argv: argument vector
-* Return: zero, or exit of 98 if fail
+* Return: return
 */
-int main(int argc, char *argv[])
+int mul(int argc, unsigned int *argv)
 {
-	int l1, l2, ln, ti, i;
-	char *a;
-	char *t;
-	char e[] = "Error\n";
+	if (argc > 2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[0] / 10 != 0 || argv[1] / 10 != 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-	if (argc != 3 || check_for_digits(argv))
-	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
-		exit(98);
-	}
-	for (l1 = 0; argv[1][l1]; l1++)
-		;
-	for (l2 = 0; argv[2][l2]; l2++)
-		;
-	ln = l1 + l2 + 1;
-	a = malloc(ln * sizeof(char));
-	if (a == NULL)
-	{
-		for (ti = 0; e[ti]; ti++)
-			_putchar(e[ti]);
-		exit(98);
-	}
-	init(a, ln - 1);
-	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
-	{
-		t = mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
-		if (t == NULL)
-		{
-			for (ti = 0; e[ti]; ti++)
-				_putchar(e[ti]);
-			free(a);
-			exit(98);
-		}
-	}
-	_print(a, ln - 1);
+	int res = argv[0] * argv[1];
+
+	printf(res);
+
 	return (0);
 }
