6876fb1bfac581a9b3834f9c6f80c40ec692efd5
leulkashay <leulkashay2012@gmail.com>
2022-10-24 12:07:32 -0700

    task 6 pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,26 @@
 #include "lists.h"
 
 /**
-*
+* pop_listint - unction that deletes the head node of a listint_t
+* @head: head of the list
+* Return:  the head node’s data (n)
 */
 
+int pop_listint(listint_t **head)
+{
+	int y_node;
+	listint_t *h;
+	listint_t *unk;
 
+	if (*head == NULL)
+		return (0);
+	unk = *head;
+	y_node = unk->n;
+	h = unk->next;
+
+	free(unk);
+
+	*head = h;
+
+	return (y_node);
+}
