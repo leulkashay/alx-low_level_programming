5fe0372040fefcc1e63c9b040fbe2624d3e2caaf
leulkashay <leulkashay2012@gmail.com>
2022-12-08 01:14:04 -0800

    7-update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -4,7 +4,7 @@
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: pointer to head of list
  * @idx: index of the list where the new node should be added
- * @n: data of the new node 
+ * @n: data of the new node
  * Return:  the address of the new node, or NULL if it failed
  */
 
