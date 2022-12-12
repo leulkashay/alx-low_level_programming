69e7db61a495b360c163f0c0583cc0955d1b14fd
leulkashay <leulkashay2012@gmail.com>
2022-12-12 06:12:27 -0800

    new

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>                                                                                                                      
+#include "main.h"
+
+/**                                                                                                                                     
+ * mul - function multiplies two integers                                                                                                     
+ * @a - integer one                                                                                                                     
+ * @b - integer two                                                                                                                     
+ *                                                                                                                                      
+ * Return - product of a and b                                                                                                                        
+ */                                                                                                                                     
+
+void mul(int a, int b)                                                                                                                  
+
+{                                                                                                                                       
+        return(a*b);                                                                                                                  
+} 
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,15 @@
+#include <stdio.h>                                                                                                                      
+#include "main.h"
+
+/**                                                                                                                                     
+ * sub - function subtracts two integers                                                                                                     
+ * @a - integer one                                                                                                                     
+ * @b - integer two                                                                                                                     
+ *                                                                                                                                      
+ * Return - difference of b from a                                                                                                                        
+ */                                                                                                                                     
+
+void sub(int a, int b)                                                                                                                  
+{                                                                                                                                       
+        return(a-b);                                                                                                              
+}
