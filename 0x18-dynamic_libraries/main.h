6a3de0eea0e8d7eb5bb2930de2df0bb3955baa25
leulkashay <leulkashay2012@gmail.com>
2022-12-14 08:03:18 -0800

    op

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -41,4 +41,10 @@ char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
 
+int find(char *str, char c);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
 #endif /* __MAIN__ */
