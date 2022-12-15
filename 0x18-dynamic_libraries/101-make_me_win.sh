7b93bc61bd148485dab4629052d99377a2946c3c
leulkashay <leulkashay2012@gmail.com>
2022-12-14 17:13:44 -0800

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q --output-document=$HOME/libtest.so https://github.com/leulkashay/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=$HOME/libtest.so
+wget -P .. https://github.com/leulkashay/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD="$PWD/../libtest.so"
